7612b20510452830da40a7d939ee69fd0d7e2480
Evert Arias <evert.arias@hotmail.com>
2020-04-09 19:54:02 -0400

    Update members page

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import styles from "./styles.module.css";
+import Members from "react-github-org-members";
 
 function MembersPage() {
+  const org_name = "LoRaFlex";
   return (
     <Layout title="Members">
       <div className={styles.membersPage}>
-        <h3>
-          There is not enough information to display at this time, check back
-          later.
-        </h3>
+        <h2 style={{ color: "#5d5d5a" }}>Our Team Members</h2>
+        <Members org={org_name} token={process.env.REACT_APP_GITHUB_TOKEN} />
       </div>
     </Layout>
   );
